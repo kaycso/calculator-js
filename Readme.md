c747f6910112877fb8f76b571028b0e9769837e4
kaycso <kaycsoliveira@hotmail.com>
2024-08-26 22:40:19 -0300

    upload

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Projeto Calculadora - Javascript
+O projeto está sendo construído afim de pôr em prática os conhecimentos adquiridos de Javascript
+
+![preview](./.github/preview.png)
+
+[clique aqui para acessar o projeto](https://kaycso.github.io/calculator-js)
+
+## Tecnologias
+- HTML
+- CSS
+- JavaScript
+
+## Necessário adicionar
+- Botão para apagar o display (C)
+- Botão para apagar número digitado por engano
+- Após realizar uma operação, poder fazer outra utilizando o resultado obtido
+- Utilizar o teclado para usar a calculadora
+- Mudar o operador no histórico caso não tenha sido digitado outro número (atualmente ele deixa os dois operadores ex.: 22+-)
--- main.js
+++ main.js
@@ -1,7 +1,61 @@
-let value1 = 0;
-let value2;
-let numberOnDisplay = [];
+const historicDisplay = document.getElementById('historic')
+const numberOnDisplay = document.getElementById('number-on-display')
+let values = []
+let operator;
 
-function addNumberToDisplay(number) {
+// operações matemáticas
+function sum(value1, value2) {
+    return value1 + value2
+}
 
+function sub(value1, value2) {
+    return value1 - value2
+}
+
+function mult(value1, value2) {
+    return value1 * value2
+}
+
+function div(value1, value2) {
+    return value1 / value2
+}
+
+// funções para interação
+function addContentToDisplay(content) {
+    if(Number(numberOnDisplay.textContent) !== 0 || content === ',') {
+        numberOnDisplay.innerHTML += content
+        return
+    }
+    
+    numberOnDisplay.innerHTML = content
+}
+
+function saveNumber(content) {
+    number = Number(numberOnDisplay.textContent)
+    values.push(number)
+    addContentToDisplay(content)
+    if (content !== '=') {
+        operator = content
+    }
+}
+
+function toHistoric() {
+    historicDisplay.textContent += numberOnDisplay.textContent
+    numberOnDisplay.textContent = 0
+}
+
+function verifyOperation() {
+    let result;
+    console.log(operator, typeof operator)
+    if (operator === '+') {
+        result = sum(values[0], values[1])
+    } else if (operator === '-') {
+        result = sub(values[0], values[1])
+    } else if (operator === '÷') {
+        result = div(values[0], values[1])
+    } else if (operator === '×') {
+        result = mult(values[0], values[1])
+    }
+
+    addContentToDisplay(result)
 }
